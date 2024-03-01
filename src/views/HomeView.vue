a47f4b463ef2ece6970669897d8963f1f26a02ca
thaislst <89035755+thaislst@users.noreply.github.com>
2024-03-01 20:27:37 -0300

    Implement filteredData function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted , watch} from 'vue';
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
 import tempApi from '@/stores/tempApi.js';
@@ -11,6 +11,12 @@ const error = ref(null);
 const date = ref();
 const SelectedDate = ref(null);
 
+onMounted(() => {
+  const startDate = new Date();
+  const endDate = new Date(new Date().setDate(startDate.getDate() + 13));
+  date.value = [startDate, endDate];
+})
+
 
 async function initialTempApi() {
   try {
@@ -21,12 +27,30 @@ async function initialTempApi() {
 }
 
 // Função para lidar com a seleção de data no VueDatePicker
-const handleDateSelection = (date) => {
-  SelectedDate.value = date;
-  console.log(date)
+async function  handleDateSelection(date) {
+  try {
+    SelectedDate.value = date;
+  } catch (err) {
+    error.value = err.message || 'Ocorreu um erro ao obter os dados da API';
+  }
 }
 
+watch(data, () => {
+  if (data.value) {
+    filteredData.value = (data.value.hourly.time).filter(item => {
+      // Converta os valores de tempo para a mesma data do SelectedDate
+      const itemDate = new Date(item);
+      return itemDate.getDate() >= (SelectedDate.value[0]).getDate() && itemDate.getDate() <= (SelectedDate.value[1]).getDate();
+    });
+  }
+});
+
 initialTempApi();
+
+// Filtrar os dados para exibir apenas os valores correspondentes ao SelectedDate
+const filteredData = ref([]);
+
+
 
 
 </script>
@@ -39,7 +63,6 @@ initialTempApi();
       :range="{ partialRange: false }"  
       @internal-model-change="handleDateSelection" 
     />
-
 
     <table class="table-fixed" v-if="data">
       <thead>
@@ -55,13 +78,17 @@ initialTempApi();
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(item, index) in data.hourly.time" :key="index">
+        <tr v-for="(item, index) in filteredData" :key="index">
+
           <td class="p-4 text-center border-2">{{formatDate(item)}}</td>
+
           <!--  verificar se os dados estão definidos -->
           <td class="p-4 text-center border-2" v-if="data.hourly.temperature2m[index]">{{ data.hourly.temperature2m[index].toFixed(1) }}</td>
           <td v-else>N/A</td> <!-- Se não estiver definido, exiba N/A -->
+
           <td class="p-4 text-center border-2" v-if="data.hourly.apparentTemperature[index]">{{ data.hourly.apparentTemperature[index].toFixed(1) }}</td>
           <td v-else>N/A</td> 
+
         </tr>  
       </tbody>
     </table>
